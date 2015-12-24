495585d727e6010a44c49b9521d521b1d117732c
Daniel Wegemer <dwegemer@seemoo.tu-darmstadt.de>
2015-12-24 10:44:52 +0100

    rewrite of nexmon.c to use structs, defined in radiotap.h

--- kernel/drivers/net/wireless/bcmdhd/nexmon.c
+++ kernel/drivers/net/wireless/bcmdhd/nexmon.c
@@ -1,48 +1,78 @@
 #include <linux/kernel.h>	/* We're doing kernel work */
 #include <linux/module.h>	/* Specifically, a module */
+#include <linux/types.h>
 #include <linux/fs.h>
 #include <asm/uaccess.h>	/* for get_user and put_user */
 #include <osl.h>
 
+#include "radiotap.h"
 #include "nexmon.h"
 
-struct sk_buff* nexmon_decode(struct sk_buff* skb)
+void
+hexdump(unsigned char *msg, int msglen) {
+    int i, col; 
+    char buf[80];
+
+    ASSERT(strlen(pfx) + 49 <= sizeof(buf));
+
+    col = 0; 
+
+    for (i = 0; i < msglen; i++, col++) {
+        if (col % 16 == 0)
+        {
+            sprintf(buf, "size %i: ", msglen);
+            //strcpy(buf, pfx);
+        }
+        sprintf(buf + strlen(buf), "%02x", msg[i]);
+        if ((col + 1) % 16 == 0)
+            printf("%s\n", buf);
+        else
+            sprintf(buf + strlen(buf), " ");
+    }    
+
+    if (col % 16 != 0)
+        printf("%s\n", buf);
+}
+
+struct sk_buff *nexmon_decode(struct sk_buff* skb)
 {
-	char radio_tap_header[15];
-	char* data;
+	char *data;
 	unsigned int data_offset;
 	unsigned int pkt_len;
-	int rssi;
-	char my_byte;
-
-	data = skb->data;
-	pkt_len = *(unsigned short*)data;
+	int rssi = 0;
+    ieee80211_radiotap_header_t radiotaph;
+    nexmon_radiotap_present_fields_t present_fields;
+	
+    data = skb->data;
+	pkt_len = *(unsigned short*) data;
 	skb_trim(skb, pkt_len);
-	//data_offset = 12+ 0x1e + 6;
-	data_offset = 12+ 0x1e + 6 + 12;
+    // offset before the 802.11 header starts
+	data_offset = 60;
 
     //skip packets which are smaller than the 802.11 header
-	if(pkt_len<24)
-		return 0;
-	my_byte = data[12+12];
-	if ((my_byte==5) || (my_byte==1))
-		return 0;
-    //more fragements? (flags)
-	if (my_byte & 4)
-		data_offset += 2;
-	rssi = data[0x12];
+	if(pkt_len < 24) {
+        return 0;
+    }
 
-	((unsigned int*)radio_tap_header)[0] = 0x000f0000; // it_version, it_pad, it_len
-	((unsigned int*)radio_tap_header)[1] = 0x2a;
-	radio_tap_header[8] = 0x10; // flags: FRAME_INC_FCS
+    radiotaph.it_version = 0x0;
+    radiotaph.it_pad = 0x0;
+    radiotaph.it_len = 0xf;
+    //present flags
+    radiotaph.it_present = 0x2a;
+    //flags: FRAME_INC_FCS 
+    present_fields.flags = (uint8_t) 0x10;
+    present_fields.padding1 = (uint8_t) 0x0;
 	// we set the frequency fix for now, this should be delivered by the firmware in the future
-	((unsigned short*)(radio_tap_header+10))[0] = 2437; // frequency
-	((unsigned short*)(radio_tap_header+10))[1] = 0x0080; // G2_SPEC
-	radio_tap_header[14] = rssi;
+    present_fields.frequency = (uint16_t) 2437;
+	// 2 GHz spectrum
+    present_fields.channel = (uint16_t) 0x0080;
+    //TODO set RSSI properly
+    present_fields.signal = (uint8_t) rssi;
 
-	skb_pull(skb,data_offset);
-	skb_push(skb,sizeof(radio_tap_header));
-	memcpy(skb->data,radio_tap_header,sizeof(radio_tap_header));
+	skb_pull(skb, data_offset);
+	skb_push(skb, sizeof(ieee80211_radiotap_header_t) + sizeof(nexmon_radiotap_present_fields_t));
+	memcpy(skb->data, &radiotaph, sizeof(ieee80211_radiotap_header_t));
+    memcpy(skb->data + sizeof(ieee80211_radiotap_header_t), &present_fields, sizeof(nexmon_radiotap_present_fields_t));
 
 	return skb;
 }
