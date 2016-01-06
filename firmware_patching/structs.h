d9e8f2eb26a28fe461c89acc206e15916d76cc15
Daniel Wegemer <daniel@wegemer.com>
2016-01-06 13:12:55 +0100

    added new structs file + cleanup bcm4339.h

--- firmware_patching/structs.h
+++ firmware_patching/structs.h
@@ -1,12 +1,19 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
+typedef struct sk_buff {
+    struct sk_buff *next;
+    struct sk_buff *prev;
+} sk_buff;
+
 struct wlc_hw_info {
     struct wlc_info *wlc;
     int gap[4];
-    void *di[6];
+    void *di[6]; // only 4 byte
     int gap2[52];
-    int maccontrol;
+    int maccontrol; // @ 0xe7
+    int gap3[18];
+    sk_buff *some_skbuff_ptr; // @ 0x134
 };
 
 struct wlc_info {
@@ -14,18 +21,17 @@ struct wlc_info {
 };
 
 struct wlc_pub {
-    int gap[10];
-    struct tunables *tunables;
-};
+    int gap1[10];
+    struct tunables *tunables; // @ 0x28
+    char gap2[187];
+    char is_amsdu; // @ 0xe7
+}__attribute__((packed));
 
 struct tunables {
-    int gap[16];
-    int rxbnd;
+    char gap[62];
+    short somebnd; // @ 0x38
+    short rxbnd; // @ 0x40
 };
 
-typedef struct sk_buff {
-    struct sk_buff *next;
-    struct sk_buff *prev;
-} sk_buff;
 
 #endif /*STRUCTS_H */
