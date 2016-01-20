d7314ab14ea2cd054b3554da96dc5be95cbf2ab5
Matthias Schulz <mschulz@seemoo.tu-darmstadt.de>
2016-01-20 19:12:18 +0100

    added a wrapper for the copied dngl_sendpkt function, which is now accessible by using dngl_sendpkt_alternative.

--- firmware_patching/dma_txfast_path/dma_txfast_hook.c
+++ firmware_patching/dma_txfast_path/dma_txfast_hook.c
@@ -3,5 +3,6 @@
 void
 *dma_txfast_hook(void *di, void *p, int commit)
 {
+	
 	return dma_txfast(di, p, commit);
 }
---
+++ firmware_patching/dma_txfast_path/dngl_sendpkt_alternative.c
@@ -0,0 +1,1 @@
+void *dngl_sendpkt_alternative(void *sdio, void *p, int chan) { return 0; }
