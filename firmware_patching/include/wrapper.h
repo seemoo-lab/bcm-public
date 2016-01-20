29e56541e12c7d0844f415540d487c270cb40879
Matthias Schulz <mschulz@seemoo.tu-darmstadt.de>
2016-01-20 18:08:18 +0100

    wrapper.ld is now created automatically from wrapper.h

--- firmware_patching/include/wrapper.h
+++ firmware_patching/include/wrapper.h
@@ -1,14 +1,14 @@
 #ifndef WRAPPER_H
 #define WRAPPER_H
 
-extern void *dma_rx(void *di);
-extern void *dma_rxfill(void *di);
-extern void *dma_txfast(void *di, void *p, int commit);
-extern void *dngl_sendpkt(void *sdio, void *p, int chan);
-extern void *wlc_mctrl_write(void *wlc_hw_info_ptr);
-extern void *wlc_bmac_mctrl(void *wlc_hw_info_ptr, int mask, int val);
-extern int printf(const char *format, ...);
-extern void *pkt_buf_get_skb(void *osh, unsigned int len);
-extern void *pkt_buf_free_skb(void *osh, void *p, int send);
+extern void *dma_rx(void *di); // 0x8c69c
+extern void *dma_rxfill(void *di); // 0x8c6cc
+extern void *dma_txfast(void *di, void *p, int commit); // 0x1844B2
+extern void *dngl_sendpkt(void *sdio, void *p, int chan); // 0x182750
+extern void *wlc_mctrl_write(void *wlc_hw_info_ptr); // 0x4df60
+extern void *wlc_bmac_mctrl(void *wlc_hw_info_ptr, int mask, int val); // 0x4F080
+extern int printf(const char *format, ...); // 0x126f0
+extern void *pkt_buf_get_skb(void *osh, unsigned int len); // 0x184F14
+extern void *pkt_buf_free_skb(void *osh, void *p, int send); // 0x184F64
 
 #endif /*WRAPPER_H*/
