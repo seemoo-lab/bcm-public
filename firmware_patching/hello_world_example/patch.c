da0c486087ef9b8c3baf054c79bd814f10b51a75
Matthias Schulz <mschulz@seemoo.tu-darmstadt.de>
2016-04-20 11:14:10 +0200

    added a hello world example patch

---
+++ firmware_patching/hello_world_example/patch.c
@@ -0,0 +1,37 @@
+#include "../include/bcm4339.h"
+#include "../include/debug.h"
+#include "../include/wrapper.h"
+#include "../include/structs.h"
+#include "../include/helper.h"
+
+/**
+ *	A simple hello world printing function
+ */
+void
+hello_world(void)
+{
+	printf("hello world!\n");			// Calls a function in ROM. Its location is defined in wrapper.h
+}
+
+/**
+ *	is called before the wlc_ucode_download function to print our hello world message
+ */
+__attribute__((naked)) void
+wlc_ucode_download_hook(void)
+{
+	asm(
+		"push {r0-r3,lr}\n"				// Push the registers that could be modified by a call to a C function
+		"bl hello_world\n"				// Call a C function
+		"pop {r0-r3,lr}\n"				// Pop the registers that were saved before
+		"b wlc_ucode_download\n"		// Call the hooked function
+		);
+}
+
+/**
+ *	Just inserted to produce an error while linking, when we try to overwrite existing memory
+ */
+void 
+dummy_180800(void)
+{
+	;
+}
---
+++ firmware_patching/hello_world_example/patcher.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python2
+
+import os
+import sys
+import time
+sys.path.append('../../buildtools/binary_patcher')
+sys.path.append('../../buildtools/elffile')
+import binary_patcher
+from binary_patcher import *
+import elffile
+
+ef = elffile.open(name="patch.elf")
+
+# This function gives us the address of a function in our patch.elf file It helps to identify where functions where automatically placed by the linker
+def getSectionAddr(name):
+	return next((header for header in ef.sectionHeaders if header.name == name), None).addr
+
+patch_firmware("../../bootimg_src/firmware/fw_bcmdhd.orig.bin", 
+    "fw_bcmdhd.bin", [
+    # The text section is always required and contains code that is called by patches and hooks but not directly placed to predefined memory locations
+    # in this example, this section contains the hello_world function.
+    #
+    # Linker output:
+	# .text           0x0000000000180030       0x1c
+	#  patch.o(.text .text.* .data .data.* .bss .bss.* .rodata .rodata.*)
+	#  .text.hello_world
+	#                 0x0000000000180030        0xc patch.o
+	#                 0x0000000000180030                hello_world
+	#  .rodata.str1.4
+	#                 0x000000000018003c       0x10 patch.o
+	#
+	ExternalArmPatch(getSectionAddr(".text"), "text.bin"),
+
+	# This function contains the hooking code for the wlc_ucode_download function
+	#
+	# Linker output:
+	# .text.wlc_ucode_download_hook
+	#                 0x0000000000180020       0x10
+	#  patch.o(.text.wlc_ucode_download_hook)
+	#  .text.wlc_ucode_download_hook
+	#                 0x0000000000180020       0x10 patch.o
+	#                 0x0000000000180020                wlc_ucode_download_hook
+	#
+	ExternalArmPatch(getSectionAddr(".text.wlc_ucode_download_hook"), "wlc_ucode_download_hook.bin"),
+
+	# We replace the only branch-link instruction to the wlc_ucode_download function with a branch-link instruction to our hook
+	BLPatch(0x1FD7F2, getSectionAddr(".text.wlc_ucode_download_hook")),
+
+	# This line replaces the firmware version string that is printed to the console on startup to identify which firmware is loaded by the driver
+	StringPatch(0x1FD31B, (os.getcwd().split('/')[-1] + " (" + time.strftime("%d.%m.%Y %H:%M:%S") + ")\n")[:52]), # 53 character string
+	])
