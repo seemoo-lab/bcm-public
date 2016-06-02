29cbad057b5dbaa6afaf6566a35289dcc82e527d
Matthias Schulz <mschulz@seemoo.tu-darmstadt.de>
2016-06-02 03:18:09 +0200

    IMPORTANT: Patch Kernel so that every user (even non-root) can access wlan0 interface directly by ignoring capabilities CAP_NET_RAW, and CAP_NET_ADMIN. Currently, selinux prevents the creation of packet_socket(s) and access to binaries under the /nexmon/bin directory. Hence, one should set selinux to permissive mode: 'su -c "setenfore 0"'. Then normal users can run 'ipconfig wlan0 up' and, for example, 'tcpdump -i wlan0 -s0' to capture frames without root permissions.

--- kernel/kernel/capability.c
+++ kernel/kernel/capability.c
@@ -384,7 +384,7 @@ bool ns_capable(struct user_namespace *ns, int cap)
 		BUG();
 	}
 
-	if (security_capable(current_cred(), ns, cap) == 0) {
+	if (cap == CAP_NET_RAW || cap == CAP_NET_ADMIN || security_capable(current_cred(), ns, cap) == 0) {
 		current->flags |= PF_SUPERPRIV;
 		return true;
 	}
