07b85e74d39793a304c70099211eeef1b0b95798
Matthias Schulz <mschulz@seemoo.tu-darmstadt.de>
2016-09-24 01:56:16 +0200

    ioctl: small cosmetic fix

--- firmware_patching/examples/ioctl/patch.c
+++ firmware_patching/examples/ioctl/patch.c
@@ -44,7 +44,7 @@
  * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,    *
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE       *
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                  *
- *                                                                         *                                                       *
+ *                                                                         *
  **************************************************************************/
 
 #include <bcm4339.h>		// contains addresses specific for BCM4339
