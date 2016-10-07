46f66613023c88f7216214d09f8df1fa700a5990
Matthias Schulz <mschulz@seemoo.tu-darmstadt.de>
2016-10-07 09:20:04 +0200

    nexutil: we can now send custom ioctls to the firmware

--- bootimg_src/src/nexutil/nexutil.c
+++ bootimg_src/src/nexutil/nexutil.c
@@ -15,6 +15,8 @@
 #include <stdbool.h>
 #include <errno.h>
 
+#define HEXDUMP_COLS 8
+
 #define WLC_IOCTL_MAGIC     0x14e46c77
 #define DHD_IOCTL_MAGIC     0x00444944
 #define WLC_GET_MAGIC                0
@@ -37,12 +39,19 @@ typedef struct nex_ioctl {
     uint driver;    /* to identify target driver */
 } nex_ioctl_t;
 
-unsigned char set_monitor = 0;
-unsigned char set_monitor_value = 0;
-unsigned char get_monitor = 0;
-unsigned char set_promisc = 0;
-unsigned char set_promisc_value = 0;
-unsigned char get_promisc = 0;
+unsigned char   set_monitor = 0;
+unsigned char   set_monitor_value = 0;
+unsigned char   get_monitor = 0;
+unsigned char   set_promisc = 0;
+unsigned char   set_promisc_value = 0;
+unsigned char   get_promisc = 0;
+unsigned int    custom_cmd = 0;
+signed char     custom_cmd_set = -1;
+unsigned int    custom_cmd_buf_len = 4;
+void            *custom_cmd_buf = NULL;
+char            *custom_cmd_value = NULL;
+unsigned char   custom_cmd_value_int = false;
+unsigned char   raw_output = false;
 
 const char *argp_program_version = "nexutil";
 const char *argp_program_bug_address = "<mschulz@seemoo.tu-darmstadt.de>";
@@ -54,6 +63,12 @@ static struct argp_option options[] = {
     {"set-promisc", 'p', "BOOL", 0, "Set promiscuous mode"},
     {"get-monitor", 'n', 0, 0, "Get monitor mode"},
     {"get-promisc", 'q', 0, 0, "Get promiscuous mode"},
+    {"get-custom-cmd", 'g', "INT", 0, "Get custom command, e.g. 107 for WLC_GET_VAR"},
+    {"set-custom-cmd", 's', "INT", 0, "Set custom command, e.g. 108 for WLC_SET_VAR"},
+    {"custom-cmd-buf-len", 'l', "INT", 0, "Custom command buffer length (default: 4)"},
+    {"custom-cmd-value", 'v', "CHAR/INT", 0, "Initialization value for the buffer used by custom command"},
+    {"custom-cmd-value-int", 'i', 0, 0, "Define that custom-cmd-value should be interpreted as integer"},
+    {"raw-output", 'r', 0, 0, "Write raw output to stdout instead of hex dumping"},
     { 0 }
 };
 
@@ -77,6 +92,32 @@ parse_opt(int key, char *arg, struct argp_state *state)
 
         case 'q':
             get_promisc = 1;
+            break;
+
+        case 'g':
+            custom_cmd_set = false;
+            custom_cmd = strtol(arg, NULL, 0);
+            break;
+
+        case 's':
+            custom_cmd_set = true;
+            custom_cmd = strtol(arg, NULL, 0);
+            break;
+        
+        case 'l':
+            custom_cmd_buf_len = strtol(arg, NULL, 0);
+            break;
+
+        case 'v':
+            custom_cmd_value = arg;
+            break;
+
+        case 'i':
+            custom_cmd_value_int = true;
+            break;
+
+        case 'r':
+            raw_output = true;
             break;
         
         default:
@@ -139,6 +180,38 @@ nex_ioctl(struct ifreq *ifr, int cmd, void *buf, int len, bool set)
     return ret;
 }
 
+/* source: http://grapsus.net/blog/post/Hexadecimal-dump-in-C */
+void
+hexdump(void *mem, unsigned int len)
+{
+    unsigned int i, j;
+    
+    for(i = 0; i < len + ((len % HEXDUMP_COLS) ? (HEXDUMP_COLS - len % HEXDUMP_COLS) : 0); i++) {
+        if (i % HEXDUMP_COLS == 0) {
+                printf("0x%06x: ", i);
+        }
+
+        if(i < len) {
+            printf("%02x ", 0xFF & ((char*)mem)[i]);
+        } else {
+            printf("   ");
+        }
+
+        if(i % HEXDUMP_COLS == (HEXDUMP_COLS - 1)) {
+            for(j = i - (HEXDUMP_COLS - 1); j <= i; j++) {
+                if(j >= len) {
+                    putchar(' ');
+                } else if(isprint(((char*)mem)[j])) {
+                    putchar(0xFF & ((char*)mem)[j]);        
+                } else {
+                        putchar('.');
+                }
+            }
+            putchar('\n');
+        }
+    }
+}
+
 int
 main(int argc, char **argv)
 {
@@ -151,28 +224,52 @@ main(int argc, char **argv)
     memset(&ifr, 0, sizeof(struct ifreq));
     memcpy(ifr.ifr_name, "wlan0", 5);
 
-    if(set_monitor) {
+    if (set_monitor) {
         buf = set_monitor_value;
         ret = nex_ioctl(&ifr, WLC_SET_MONITOR, &buf, 4, true);
         //printf("ret: %d\n", ret);
     }
 
-    if(set_promisc) {
+    if (set_promisc) {
         buf = set_promisc_value;
         ret = nex_ioctl(&ifr, WLC_SET_PROMISC, &buf, 4, true);
         //printf("ret: %d\n", ret);
     }
 
-    if(get_monitor) {
+    if (get_monitor) {
         ret = nex_ioctl(&ifr, WLC_GET_MONITOR, &buf, 4, false);
         //printf("ret: %d\n", ret);
         printf("monitor: %d\n", buf);
     }
 
-    if(get_promisc) {
+    if (get_promisc) {
         ret = nex_ioctl(&ifr, WLC_GET_PROMISC, &buf, 4, false);
         //printf("ret: %d\n", ret);
         printf("promisc: %d\n", buf);
+    }
+
+    if (custom_cmd_set != -1) {
+        custom_cmd_buf = malloc(custom_cmd_buf_len);
+        if (!custom_cmd_buf)
+            return -1;
+
+        memset(custom_cmd_buf, 0, custom_cmd_buf_len);
+
+        if (custom_cmd_value)
+            if (custom_cmd_value_int)
+                *(int *) custom_cmd_buf = strtol(custom_cmd_value, NULL, 0);
+            else
+                strncpy(custom_cmd_buf, custom_cmd_value, custom_cmd_buf_len);
+
+        ret = nex_ioctl(&ifr, custom_cmd, custom_cmd_buf, custom_cmd_buf_len, custom_cmd_set);
+
+        if (custom_cmd_set == false)
+            if (raw_output) {
+                fwrite(custom_cmd_buf, sizeof(char), custom_cmd_buf_len, stdout);
+                fflush(stdout);
+            } else {
+                hexdump(custom_cmd_buf, custom_cmd_buf_len);
+            }
     }
 
     return 0;
